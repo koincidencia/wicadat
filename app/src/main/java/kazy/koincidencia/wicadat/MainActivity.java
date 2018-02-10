55cca0e735fe4d3392802907fb2b87070bc2379e
koincidencia <kazykazy91@gmail.com>
2018-02-10 11:30:02 +0100

    add fields

--- app/src/main/java/kazy/koincidencia/wicadat/MainActivity.java
+++ app/src/main/java/kazy/koincidencia/wicadat/MainActivity.java
@@ -2,12 +2,24 @@ package kazy.koincidencia.wicadat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView rpmValue;
+    TextView voltageValue;
+    TextView tempValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rpmValue = (TextView) findViewById(R.id.rpmValue);
+        voltageValue = (TextView) findViewById(R.id.voltageValue);
+        tempValue = (TextView) findViewById(R.id.tempValue);
+
+        rpmValue.setText("0");
+        voltageValue.setText("12.1");
+        tempValue.setText("23");
     }
 }
